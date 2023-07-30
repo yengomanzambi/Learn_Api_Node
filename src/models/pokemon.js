895d148e1eb46c8d458a8861f1ecf3c8892bc2de
gracius yengo <yengogracius@gmail.com>
2023-07-31 00:58:19 +0200

    feat : add validation sequelizein field hp and picture

--- src/models/pokemon.js
+++ src/models/pokemon.js
@@ -9,10 +9,31 @@ module.exports = (sequelize, DataTypes) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        notEmpty:{msg:"le champs ne peu pas etre vide"},
+        notNull:{msg:"ce champs est obligatoire svp"}
+  
+        }
+    
+        
+     
     },
     hp: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate:{
+        isInt:{msg:"veuillez entrer un nombre svp entier"},
+        notNull:{msg:"ce champs est obligatoire svp"},
+        min:{
+          args:[0],
+          msg:"la taille du point de vie doit etre superieur ou égale à 0"
+        },
+        max:{
+          args:[999],
+          msg:"la taille du point de vie doit etre inferieur ou égale à 999"
+        }
+  
+      }
       
     },
     cp: {
@@ -20,14 +41,23 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       validate:{
       isInt:{msg:"veuillez entrer un nombre svp entier"},
-      notNull:{msg:"ce champs est obligatoire svp"}
-
+      notNull:{msg:"ce champs est obligatoire svp"},
+      min:{
+        args:[0],
+        msg:"ce champs doit etre superieur ou égale à 0"
+      },
+      max:{
+        args:[99],
+        msg:"ce champs doit étre inferieur ou égale à 99"
       }
-      
+      }
     },
     picture: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate:{
+        isUrl:{msg:"veuillez entrez un url valide"}
+      }
     },
     types: {
       type: DataTypes.STRING,
